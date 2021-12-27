290211abe44d9024e091175a391ef890c1e95f41
ShadowFriend1 <jack.negus3@gmail.com>
2021-12-27 22:12:18 +0000

    Added support for kfold analysis including outputting plot of mean errors

--- Regression_Algorithm.py
+++ Regression_Algorithm.py
@@ -42,61 +42,61 @@ def shuffling(df):
 
 
 def pca(df):
-    # if possible to auto generate X,y with all features instead of manually picking featuers for X
-    X = df[['open', 'high', 'low', 'close', 'volume', 'divCash', 'splitFactor']].values.astype(np.float32)
+    # if possible to auto generate x,y with all features instead of manually picking features for x
+    x = df[['open', 'high', 'low', 'close', 'volume', 'divCash', 'splitFactor']].values.astype(np.float32)
 
     sc = StandardScaler()
-    X_scaled = sc.fit_transform(X)
+    x_scaled = sc.fit_transform(x)
 
     # pca = PCA(n_components=None) #shows that only need 1 component to capture 100% of the data
-    pca = PCA(n_components=None)
-    pca.fit(X_scaled)
+    pca_bot = PCA(n_components=None)
+    pca_bot.fit(x_scaled)
 
     # Get the eigenvalues
     print("Eigenvalues:")
-    print(pca.explained_variance_)
+    print(pca_bot.explained_variance_)
     print()
 
     # Get explained variances
     print("Variances (Percentage):")
-    print(pca.explained_variance_ratio_ * 100)
+    print(pca_bot.explained_variance_ratio_ * 100)
     print()
 
     # Make the scree plot
-    plt.plot(np.cumsum(pca.explained_variance_ratio_ * 100))
+    plt.plot(np.cumsum(pca_bot.explained_variance_ratio_ * 100))
     plt.xlabel("Number of components (Dimensions)")
     plt.ylabel("Explained variance (%)")
 
 
 def trained_pca(df):
-    X = df[['open', 'high', 'low', 'close', 'volume', 'divCash', 'splitFactor']].values.astype(np.float32)
+    x = df[['open', 'high', 'low', 'close', 'volume', 'divCash', 'splitFactor']].values.astype(np.float32)
     y = df[['close']].values.astype(np.float32)
 
     sc = StandardScaler()
-    X_scaled = sc.fit_transform(X)
-    pca = PCA(n_components=2)
-    X_pca = pca.fit_transform(X_scaled)
+    x_scaled = sc.fit_transform(x)
+    pca_bot = PCA(n_components=2)
+    x_pca = pca_bot.fit_transform(x_scaled)
 
-    X_train, X_test, y_train, y_test = train_test_split(X_pca, y, test_size=0.2, random_state=42)
+    x_train, x_test, y_train, y_test = train_test_split(x_pca, y, test_size=0.2, random_state=42)
 
     # standardises the data - will not do much as data is already similar in size
 
     model = LinearRegression()
-    model.fit(X_train, y_train)
+    model.fit(x_train, y_train)
 
     # fitting and testing the model
-    pred = model.predict(X_test)  # usually only out by 0.0005
+    pred = model.predict(x_test)  # usually only out by 0.0005
 
     # shows no correlation between components
-    # X_pca = pd.DataFrame(X_pca)
+    # x_pca = pd.DataFrame(x_pca)
     # fig = plt.figure(figsize=(10, 8))
-    # sns.heatmap(X_pca.corr(), annot=True)
+    # sns.heatmap(x_pca.corr(), annot=True)
 
-    return model, X_train, X_test, y_test, pred
+    return model, x_train, x_test, y_test, pred
 
 
 def train_k_fold(df, k):
-    X = df[['open', 'high', 'low', 'close', 'volume', 'divCash', 'splitFactor']].values.astype(np.float32)
+    x = df[['open', 'high', 'low', 'close', 'volume', 'divCash', 'splitFactor']].values.astype(np.float32)
     y = df[['close']].values.astype(np.float32)
 
     # standardises the data - will not do much as data is already similar in size
@@ -110,12 +110,12 @@ def train_k_fold(df, k):
     worst_score = 0
     average_score = 0
     scores = []
-    for train_index, validate_index in kf.split(X, y):
-        model.fit(X[train_index], y[train_index])
+    for train_index, validate_index in kf.split(x, y):
+        model.fit(x[train_index], y[train_index])
         y_test = y[validate_index]
-        pred = model.predict(X[validate_index])
+        pred = model.predict(x[validate_index])
         score = np.sqrt(metrics.mean_squared_error(pred, y_test))
-        print(f"Fold:  #{fold}, Training Size: {len(X[train_index])}, Validation Size: {len(y[validate_index])}")
+        print(f"Fold:  #{fold}, Training Size: {len(x[train_index])}, Validation Size: {len(y[validate_index])}")
         print("Mean Squared error: {}".format(score))
         if score < best_score:
             best_score = score
@@ -147,25 +147,25 @@ def train_k_fold(df, k):
 
 
 def train(df):
-    X = df[['open', 'high', 'low', 'close', 'volume', 'divCash', 'splitFactor']].values.astype(np.float32)
+    x = df[['open', 'high', 'low', 'close', 'volume', 'divCash', 'splitFactor']].values.astype(np.float32)
     y = df[['close']].values.astype(np.float32)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
     # standardises the data - will not do much as data is already similar in size
 
     model = LinearRegression()
-    model.fit(X_train, y_train)
+    model.fit(x_train, y_train)
 
     # fitting and testing the model
-    pred = model.predict(X_test)  # usually only out by 0.0005
+    pred = model.predict(x_test)  # usually only out by 0.0005
 
     # shows correlation between the first 4 components
-    # X = pd.DataFrame(X)
+    # x = pd.DataFrame(x)
     # fig = plt.figure(figsize=(10, 8))
-    # sns.heatmap(X.corr(), annot=True)
+    # sns.heatmap(x.corr(), annot=True)
 
-    return model, X_train, X_test, y_test, pred
+    return model, x_train, x_test, y_test, pred
 
 
 def output(y_test, pred):
@@ -186,33 +186,33 @@ def output(y_test, pred):
 
 def correlation_test():
     dataframe = read('GOOG.csv')  # returns dataframe
-    # heatmap(dataframe) #adjvolume and volume are not corrolated to other covariates
+    # heatmap(dataframe) #adjvolume and volume are not correlated to other co-variates
     pca(dataframe)  # only need the first component to capture almost 100% of the data
 
 
-def runModel():
+def run_model():
     dataframe = read('GOOG.csv')  # returns dataframe
     dataframe = shuffling(dataframe)  # returns dataframe
-    trained_model = train(dataframe)  # return model, X_train, X_test, y_test, pred
+    trained_model = train(dataframe)  # return model, x_train, x_test, y_test, pred
     output(trained_model[3], trained_model[4])
 
 
-def run_PCA_Model():
+def run_pca_model():
     dataframe = read('GOOG.csv')  # returns dataframe
     dataframe = shuffling(dataframe)  # returns dataframe
-    trained_model = trained_pca(dataframe)  # return model, X_train, X_test, y_test, pred
+    trained_model = trained_pca(dataframe)  # return model, x_train, x_test, y_test, pred
     output(trained_model[3], trained_model[4])
 
 
-def run_K_Fold_model():
+def run_k_fold_model():
     dataframe = read('GOOG.csv')  # returns dataframe
     dataframe = shuffling(dataframe)  # returns dataframe
-    train_k_fold(dataframe, 10)  # return model, X_train, X_test, y_test, pred
+    train_k_fold(dataframe, 10)  # return model, x_train, x_test, y_test, pred
 
 
 if __name__ == "__main__":
     # correlation_test()
-    # runModel()
-    # run_PCA_Model()  # has a higher R squared value but no corrleation, suggesting hidden variables???
-    run_K_Fold_model()
+    # run_model()
+    # run_pca_model()  # has a higher R squared value but no correlation, suggesting hidden variables???
+    run_k_fold_model()
     plt.show()
